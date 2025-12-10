f6fad9592444c0dd3102bc8d8d2465d78dd9ea40
Baylsan-Ml <bailsanmletat99@gmail.com>
2025-12-11 00:45:52 +0200

    Router file name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
-import router from '../oute.jsx'
+import router from '../Route.jsx'
 
 export default function App() {
   return (
