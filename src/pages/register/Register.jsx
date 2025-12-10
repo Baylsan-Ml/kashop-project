5916a778b5fb73c02dd6ef0160ea1493589147c0
Baylsan-Ml <bailsanmletat99@gmail.com>
2025-12-10 20:48:29 +0200

    Register form

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,36 +1,41 @@
-import { Box , Button, TextField, Typography} from '@mui/material'
-import axios from 'axios';
-
 import React from 'react'
+import { Box, Button, TextField, Typography } from '@mui/material'
+import axios from 'axios'
 import { useForm } from 'react-hook-form'
 
+export default function Regiester() {
 
-export default function Register() {
+  const {register, handleSubmit} = useForm({})
+  
+  const registerForm = async (values)=>{
+  
+  console.log(values);
+  
+  try{
+  const response = await axios.post(`https://knowledgeshop.runasp.net/api/Auth/Account/Register`, values);
 
-  const {register, handleSubmit}=useForm({});
+  console.log(response);
 
-  const registerForm= async(values)=>{
-    try{
-      const response= await axios.post(`http://knowledgeshop.runasp.net/api/Auth/Account/Register`, values);
+  }catch(err){
+  console.log(err);}
+}
+  
 
-      console.log(response);
-
-    }catch(err){
-      console.log(err);
-    }
-  }
   return (
-    <Box className='Register-form' sx={{display:'flex',flexDirection:'column', alignItems:'center', justifyContent:'center'}}>
-      <Typography variant='h1' sx={{textAlign: 'center', mt:3 }}>Register Page</Typography>
+   
+<Box className ="register-form" >
+<Typography variant='h1'sx={{textAlign: 'center', mt:3 }} >Register Page</Typography>
 
-      <Box onSubmit={handleSubmit(registerForm)} component={"form"} mt={5} mb={5} sx={{display:'flex', flexDirection:'column', gap:3, width: '40%'}}>
-        <TextField {...register('userName')} label='user name' variant="outlined"></TextField>
-        <TextField {...register('fullName')} label='full name'  variant="outlined"></TextField>
-        <TextField {...register('email')} label='user email' variant="outlined"></TextField>
-        <TextField {...register('password')} label='password' variant="outlined"></TextField>
-        <TextField {...register('phoneNumber')} label='phone number' variant="outlined"></TextField>
-        <Button variant="contained" type='submit' sx={{backgroundColor:'InfoText'}}>Register</Button>
-      </Box>
-    </Box>
+<Box onSubmit={handleSubmit(registerForm)} component={"form"} 
+sx={{ display: 'flex', flexDirection: 'column', gap: 3, mt: 5, alignItems: 'center'}}>
+<TextField label="user name" {...register('userName')} sx={{width:'40%'}} variant="outlined"/>
+<TextField label="Full Name" {...register('fullName')} sx={{width:'40%'}} variant="outlined"/>
+<TextField label="user email" {...register('email')} sx={{width:'40%'}} variant="outlined"/>
+<TextField label="password" {...register('password')} sx={{width:'40%'}} variant="outlined"/>
+ <TextField label="phone number" {...register('phoneNumber')} sx={{width:'40%'}} variant="outlined" />
+ <Button variant="contained" type="submit"  sx={{backgroundColor:'InfoText', width:'40%'}}>Register</Button>
+ </Box>
+</Box>
+
   )
 }
