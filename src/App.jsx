b984067c3ba227f7662c9c0b0ab64620854020ce
Baylsan-Ml <bailsanmletat99@gmail.com>
2025-12-11 00:41:32 +0200

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
-import router from '../Route'
+import router from '../route'
 
 export default function App() {
   return (
