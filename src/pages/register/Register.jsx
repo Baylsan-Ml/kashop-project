5b557e54b4c65968ad43be24f9a84b2e7e7961ae
Baylsan-Ml <bailsanmletat99@gmail.com>
2025-12-13 23:58:20 +0200

    client side validation

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,15 +2,25 @@ import React from 'react'
 import { Box, Button, TextField, Typography } from '@mui/material'
 import axios from 'axios'
 import { useForm } from 'react-hook-form'
+import * as yup from 'yup';
+import { yupResolver } from "@hookform/resolvers/yup";
+import { RegiesterSchema } from '../../validation/RegisterSchema';
+
+
+
+
 
 export default function Regiester() {
 
-  const {register, handleSubmit} = useForm({})
+  const {register, handleSubmit, formState:{errors}} = useForm({
+    resolver:yupResolver(RegiesterSchema),
+    mode: 'onBlur'
+  })
   
   const registerForm = async (values)=>{
   
+
   console.log(values);
-  
   try{
   const response = await axios.post(`https://knowledgeshop.runasp.net/api/Auth/Account/Register`, values);
 
@@ -28,11 +38,16 @@ export default function Regiester() {
 
 <Box onSubmit={handleSubmit(registerForm)} component={"form"} 
 sx={{ display: 'flex', flexDirection: 'column', gap: 3, mt: 5, alignItems: 'center'}}>
-<TextField label="user name" {...register('userName')} sx={{width:'40%'}} variant="outlined"/>
-<TextField label="Full Name" {...register('fullName')} sx={{width:'40%'}} variant="outlined"/>
-<TextField label="user email" {...register('email')} sx={{width:'40%'}} variant="outlined"/>
-<TextField label="password" {...register('password')} sx={{width:'40%'}} variant="outlined"/>
- <TextField label="phone number" {...register('phoneNumber')} sx={{width:'40%'}} variant="outlined" />
+<TextField label="user name" {...register('userName')} sx={{width:'40%'}} variant="outlined"
+error={errors.userName} helperText={errors.userName?.message}/>
+<TextField label="Full Name" {...register('fullName')} sx={{width:'40%'}} variant="outlined"
+error={errors.fullName} helperText={errors.fullName?.message}/>
+<TextField label="user email" {...register('email')} sx={{width:'40%'}} variant="outlined"
+error={errors.email} helperText={errors.email?.message}/>
+<TextField label="password" {...register('password')} sx={{width:'40%'}} variant="outlined"
+error={errors.password} helperText={errors.password?.message}/>
+ <TextField label="phone number" {...register('phoneNumber')} sx={{width:'40%'}} variant="outlined" 
+ error={errors.phoneNumber} helperText={errors.phoneNumber?.message}/>
  <Button variant="contained" type="submit"  sx={{backgroundColor:'InfoText', width:'40%'}}>Register</Button>
  </Box>
 </Box>
---
+++ src/validation/RegisterSchema.js
@@ -0,0 +1,15 @@
+import * as yup from 'yup';
+
+
+export const RegiesterSchema = yup.object({
+  fullName:yup.string().required("FullName is Required"),
+  email:yup.string().email("Invalid Email Format").required("Email is Required"),
+  userName:yup.string().matches(/^[a-zA-Z0-9._-]+$/, "Invalid UserName").min(4, "UserName must be at least 4 Characters")
+  .required("UserName is Required"),
+  phoneNumber:yup.string().required("PhoneNumber is Requierd"),
+  password:yup.string().required("Password is Required").min(8, "Password must be at least 8 Characters")
+  .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+  .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+  .matches(/\d/, "Password must contain at least one number")
+  .matches(/[@!#$*&?]/, "Password must be at least one special Characters")
+})
