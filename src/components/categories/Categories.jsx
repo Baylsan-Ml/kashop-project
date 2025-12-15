287079d418ec4f20f2f26fecd7ff1ea1b498fa6a
Baylsan-Ml <bailsanmletat99@gmail.com>
2025-12-16 01:00:13 +0200

    create categories page

---
+++ src/components/categories/Categories.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+import axiosInstance from '../../Api/axiosInstance';
+import { Box, Card, Container, Grid, Typography } from '@mui/material';
+
+export default function Categories() {
+
+    const[categories, setCategoris]=useState([]);
+
+    const getCategories= async()=>{
+        try{
+            const response= await axiosInstance.get(`/Categories`)
+            setCategoris(response.data);
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+
+    useEffect(()=>{
+        getCategories();
+    }, [])
+  return (
+    <>
+
+    <Box p={3}>
+        <Typography component={'h2'} variant='h4' m={3} >Categories</Typography>
+        <Container maxWidth='xl'>
+        <Grid container  sx={{textAlign:'center'}}>
+            {categories.map((category)=>
+      <Grid item key={category.id} size={{sx:12, sm:6 , md:4, lg:3}} sx={{p:4}}>
+             <Card  sx={{p:4}}>{category.name}</Card> 
+            </Grid>
+      )
+      }
+        </Grid>
+        </Container>
+    </Box>
+      
+    </>
+  )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import { Typography } from '@mui/material'
 import React from 'react'
+import Categories from '../../components/categories/Categories'
 
 export default function Home() {
   return (
     <>
-    <Typography sx={{background: 'grey', fontSize: '50px' }} mt={'20px'} component="h1">This is Home</Typography >
+    <Categories />
     </>
   )
 }
