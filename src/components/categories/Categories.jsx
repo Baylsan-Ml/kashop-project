3c95e6b994ef8d91ad0365d1313cd12cb503e9ad
Baylsan-Ml <bailsanmletat99@gmail.com>
2025-12-16 15:08:25 +0200

    Starting Categories styling

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -23,12 +23,12 @@ export default function Categories() {
     <>
 
     <Box p={3} sx={{textAlign:'center'}}>
-        <Typography component={'h2'} variant='h4' m={3} >Categories</Typography>
+        <Typography component={'h2'} variant='h4' m={3} sx={{textShadow: '2px 2px 2px rgba(0,0,0,0.3)'}} >Categories</Typography>
         <Container maxWidth='xl'>
-        <Grid container  sx={{textAlign:'center'}}>
+        <Grid container sx={{textAlign:'center',}}>
             {categories.map((category)=>
       <Grid item key={category.id} size={{sx:12, sm:6 , md:4, lg:3}} sx={{p:4}}>
-             <Card  sx={{p:4, }}>{category.name}</Card> 
+             <Card sx={{p:3, backgroundColor:'#f2f6ff',  cursor: 'pointer', width:'100%',}}>{category.name}</Card> 
             </Grid>
       )
       }
