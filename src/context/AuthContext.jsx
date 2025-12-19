106a125442a3ded9e342723c38640909b445d059
Baylsan-Ml <bailsanmletat99@gmail.com>
2025-12-19 23:40:54 +0200

    create AuthContext

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+
+export const AuthContext= createContext();
+
+export default function AuthContextProvider({children}){
+
+    const [token, setToken]=useState(null);
+
+    const logout= ()=>{
+        localStorage.removeItem('token');
+        setToken(null);
+    }
+
+    return <AuthContext.Provider value={{token, setToken, logout}}>
+        {children}
+        </AuthContext.Provider>
+        
+}
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,13 +1,20 @@
-import { Box, Button, TextField, Typography, Link } from '@mui/material'
+import { Box, Button, TextField, Typography, Link, CircularProgress } from '@mui/material'
 import axios from 'axios'
 import { useForm } from 'react-hook-form'
 import {Link as RouterLink } from 'react-router-dom'
 import '../sendCode/SendCode.jsx'
 import { useNavigate } from 'react-router-dom';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup'
+import { SendCodeSchema } from '../../validation/SendCodeSchema.js'
+import axiosInstance from '../../Api/axiosInstance.js'
 
 
 export default function Login() {
-   const {register, handleSubmit} = useForm({})
+    const {register, handleSubmit, formState:{errors, isSubmitting}}= useForm({
+       resolver:yupResolver(SendCodeSchema),
+       mode:'onBlur'
+     })
     const navigate = useNavigate();
     const loginForm = async (values)=>{
     // console.log(values);
@@ -15,6 +22,7 @@ export default function Login() {
     const response = await axiosInstance.post(`/Auth/Account/Login`, values);
     if(response.status==200){
       console.log(response);
+      navigate('/home');
       localStorage.setItem("token", respoose.data.accessToken);
     }
   
@@ -31,7 +39,9 @@ export default function Login() {
   sx={{ display: 'flex', flexDirection: 'column', gap: 3, mt: 5, alignItems: 'center'}}>
   <TextField label="user email" {...register('email')} sx={{width:'40%'}} variant="outlined"/>
   <TextField label="password" {...register('password')} sx={{width:'40%'}} variant="outlined"/>
-   <Button variant="contained" type="submit"  sx={{backgroundColor:'InfoText', width:'40%'}}>Login</Button>
+   <Button variant="contained" type="submit"  disabled={isSubmitting} sx={{backgroundColor:'InfoText', width:'40%'}}>
+     {isSubmitting? <CircularProgress />: 'Login'}
+    </Button>
    <Link component={RouterLink} to={'/sendCode'}>Forget Your Password?</Link>
    </Box>
   </Box>
