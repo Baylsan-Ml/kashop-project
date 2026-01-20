b40f2ac3376b67fcd3facf5cbdac37860fa67ed7
Baylsan-Ml <bailsanmletat99@gmail.com>
2026-01-21 00:47:06 +0200

    refresh tokens

--- src/Api/axiosAuthInstance.js
+++ src/Api/axiosAuthInstance.js
@@ -3,7 +3,14 @@ import useAuthStore from "../store/authStore";
 
 const axiosAuthInstance = axios.create({
   baseURL: 'https://knowledgeshop.runasp.net/api',
+  withCredentials:true,
 });
+
+
+const axiosRefresh = axios.create({
+   baseURL: 'https://knowledgeshop.runasp.net/api',
+  withCredentials:true,
+})
 
  axiosAuthInstance.interceptors.request.use((config)=>{
     const{token}= useAuthStore.getState();
@@ -12,5 +19,26 @@ const axiosAuthInstance = axios.create({
   return config;
  })
 
+axiosAuthInstance.interceptors.response.use((response)=>{
+  console.log(response)
+  return response;
+}, async(error)=>{
+  const originalRequist=error.config;
+  if(error.response.status=== 401 && !originalRequist._retry){
+    !originalRequist._retry=== true;
 
+    try{
+      const refreshResponse= await axiosRefresh.post(`/auth/Account/RefreshToken`,{});
+      const newAccessToken = refreshResponse.data.accessToken;
+      useAuthStore.getState().setToken(newAccessToken);
+      originalRequist.headers["Authorization"]= `Bearer ${newAccessToken}`
+      return axiosAuthInstance(originalRequist);
+    }catch(refreshError){
+      useAuthStore.getState().logout();
+      return Promise.reject(refreshError)
+    }
+  }
+
+  return Promise.reject(error);
+})
  export default axiosAuthInstance;
